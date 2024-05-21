e94d6569493f3afebdc44f91ba147d018c9e7292
Carotus <120000954+Carotus@users.noreply.github.com>
2024-05-21 13:29:06 +0200

    first push

---
+++ 3 days to live/projectData/HomerVars.js
@@ -0,0 +1,105 @@
+/**
+* javascript variables for the Homer project:
+* New Project
+* Homer - The Story Flow Editor.
+* Copyright (c)2021-2024. Open Lab s.r.l - Florence, Italy
+* Developer: Pupunzi (Matteo Bicocchi)
+*
+**/
+
+class HomerVars {
+
+      static hasSpark = false;
+      static hasSeed = false;
+      static hasBrush = false;
+      static hasLantern = false;
+
+};
+
+class HomerActors {
+
+      static YOU = "You";
+      static THE_GUY_WHO_MADE_THIS_SHIT = "The guy who made this shit";
+      static THE_CANVAS_BEARER = "The canvas bearer";
+      static GUARD = "Guard";
+      static THE_LANTERN_BEARER = "The lantern bearer";
+      static THE_TRUTH_BEARER = "The truth bearer";
+      static THE_NARRATOR = "The narrator";
+      static THE_PRISONER = "The prisoner";
+
+}
+
+class HomerMeta {
+      static FLOW_STATE = {
+          IDEA : 'IDEA',
+          NOTES : 'NOTES',
+          DRAFT : 'DRAFT',
+          EDITOR : 'EDITOR',
+          FINAL : 'FINAL',
+      };
+      static MOOD = {
+          NEUTRAL : 'NEUTRAL',
+          HAPPY : 'HAPPY',
+          VERY_HAPPY : 'VERY_HAPPY',
+          SAD : 'SAD',
+          ANGRY : 'ANGRY',
+          AMAZED : 'AMAZED',
+      };
+      static EXPRESSION = {
+          RELAXED : 'RELAXED',
+          GRINNING : 'GRINNING',
+          PENSIVE : 'PENSIVE',
+          FROWNING : 'FROWNING',
+          CRYING : 'CRYING',
+          ENRAGED : 'ENRAGED',
+      };
+      static BALLOON_TYPE = {
+          SPEECH : 'SPEECH',
+          WHISPER : 'WHISPER',
+          THOUGHT : 'THOUGHT',
+          SCREAM : 'SCREAM',
+          SLEEP : 'SLEEP',
+          SING_SONG : 'SING_SONG',
+      };
+      static LOCATION = {
+          HOME : 'HOME',
+          OUTSIDE : 'OUTSIDE',
+          OFFICE : 'OFFICE',
+          PARK : 'PARK',
+          SEA : 'SEA',
+          MOUNTAIN : 'MOUNTAIN',
+      };
+      static CAMERA_DIRECTION = {
+          CLOSE_UP : 'CLOSE_UP',
+          ANGLE_ON : 'ANGLE_ON',
+          OFF_SCREEN : 'OFF_SCREEN',
+          FADE_OUT : 'FADE_OUT',
+          FADE_IN : 'FADE_IN',
+          VOICE_OVER : 'VOICE_OVER',
+      };
+      static LIGHTNING = {
+          KEY : 'KEY',
+          BACK : 'BACK',
+          PRACTICAL : 'PRACTICAL',
+          HARD : 'HARD',
+          SOFT : 'SOFT',
+          CHIAROSCURO : 'CHIAROSCURO',
+      };
+
+}
+
+class HomerLabels {
+
+
+}
+
+
+class HomerFlowSlugs {
+      static INTRO = "Intro";
+      static THE_FIRST_DAY = "The first day";
+      static THE_SECOND_DAY = "The second day";
+      static THE_THIRD_DAY = "The third day";
+      static THE_PUNISHMENT = "The punishment";
+      static THE_EPILOGUE = "The epilogue";
+ }
+
